ec9fa2b0de62197599868ea902ebe560fe2fb8b2
DonVerga <fabian.becker@students.fhnw.ch>
2017-11-27 21:42:41 +0100

    add database

---
+++ IP5_App/Assets/dbManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+
+public class dbManager : MonoBehaviour {
+
+    private String connectionString;
+
+	// Use this for initialization
+	void Start () {
+        connectionString = "URI=file:" + Application.dataPath + "/AugmentedDatabase.db";
+        getName();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void getName() {
+using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = " select * from Player;";
+                dbCmd.CommandText = sqlQuery;
+
+                using(IDataReader reader = dbCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log(reader.GetString(1));
+                    }
+                    dbConnection.Close();
+                    reader.Close();
+
+                }
+            }
+        }
+    }
+}
