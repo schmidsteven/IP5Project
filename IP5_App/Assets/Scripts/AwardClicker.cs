de4b3438a7e3b034b65482b7336213dc7e5e6e3f
StevenSchmid <steven_schmid1155@hotmail.com>
2017-11-27 18:05:11 +0100

    Script Spawn

--- IP5_App/Assets/Scripts/AwardClicker.cs
+++ IP5_App/Assets/Scripts/AwardClicker.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnScript : MonoBehaviour {
+public class AwardClicker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ IP5_App/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    int count = 0;
+    public GameObject completedMarker1;
+
+	// Use this for initialization
+	void Start () {
+
+        InvokeRepeating("spwan", 0, 1000000);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "Diamondo")
+                {
+                    Destroy(completedMarker1, 0.4f);
+                    count++;
+                }
+            }
+        }
+    }
+    void spwan()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(-30.0f, 30.0f), Random.Range(-20.0f, 20.0f), Random.Range(5.0f, 5.0f));
+        Instantiate(completedMarker1, randomPosition, Quaternion.Euler(0, Random.Range(-110.0f, 110.0f), 0));
+    }
+}
+
