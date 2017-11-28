3751c231c3c1bd859c8c404f7c30831eece4cff7
schmidsteven <31855007+schmidsteven@users.noreply.github.com>
2017-11-28 12:01:34 +0100

    x
    x

---
+++ IP5_App/Assets/Scripts/StartScene/OpenFHNW.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenFHNW : MonoBehaviour {
+
+
+    public void OpenURL()
+    {
+        Application.OpenURL("http://fhnw.ch");
+        Debug.Log("is this working?");
+    }
+
+}
+
